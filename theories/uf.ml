6ff89ed6e095c8d84b49024c7a83545c20161468
Evan Williams <evanawilliams@live.com>
2026-06-03 22:56:53 +0800

    😊

--- theories/uf.ml
+++ theories/uf.ml
@@ -33,6 +33,10 @@ module Atom = struct
   include functor Hashable.Make
 
   let create_unregistered () = None
+
+  (* Canonical form: smaller id first, so Eq(a,b) = Eq(b,a). *)
+  let eq a b = if Term_id.( <= ) a b then Eq (a, b) else Eq (b, a)
+  let neq a b = if Term_id.( <= ) a b then Neq (a, b) else Neq (b, a)
 end
 
 module Atom_data = struct
@@ -49,6 +53,8 @@ module Undo = struct
         { child : Term_id.t
         ; old_parent : Term_id.t
         ; old_rank : int
+        (* The eq var that caused this merge, for conflict explanation. *)
+        ; reason_var : int
         }
     | Class_list of
         { term : Term_id.t
@@ -61,11 +67,18 @@ module Undo = struct
     }
 end
 
+type asserted =
+  { a : Term_id.t
+  ; b : Term_id.t
+  ; var : int
+  ; decision_level : int
+  }
+
 type t =
   { terms : Term.t Vec.Value.t
   ; registry : Registry.t
-  ; mutable asserted_eqs : (Term_id.t * Term_id.t * int) list
-  ; mutable asserted_neqs : (Term_id.t * Term_id.t * int) list
+  ; mutable asserted_eqs : asserted list
+  ; mutable asserted_neqs : asserted list
   ; mutable undo_trail : Undo.t list
   }
 
@@ -80,9 +93,7 @@ let create () =
 
 let new_term t ~def =
   let id = Vec.Value.length t.terms in
-  Vec.Value.push
-    t.terms
-    { Term.id; def; parent = id; rank = 0; class_list_next = id };
+  Vec.Value.push t.terms { Term.id; def; parent = id; rank = 0; class_list_next = id };
   id
 ;;
 
@@ -90,11 +101,11 @@ let new_const t = new_term t ~def:Const
 let new_app t ~func ~args = new_term t ~def:(App { func; args })
 
 let register_eq t ~lhs ~rhs =
-  Registry.register t.registry ~atom:(Atom.Eq (lhs, rhs)) ~data:()
+  Registry.register t.registry ~atom:(Atom.eq lhs rhs) ~data:()
 ;;
 
 let register_neq t ~lhs ~rhs =
-  Registry.register t.registry ~atom:(Atom.Neq (lhs, rhs)) ~data:()
+  Registry.register t.registry ~atom:(Atom.neq lhs rhs) ~data:()
 ;;
 
 let get_term t id = Vec.Value.get t.terms id
@@ -110,7 +121,7 @@ let record_undo t ~decision_level kind =
   t.undo_trail <- { Undo.decision_level; kind } :: t.undo_trail
 ;;
 
-let union t ~decision_level a b =
+let union t ~decision_level ~reason_var a b =
   let ra = find t a
   and rb = find t b in
   if Term_id.equal ra rb
@@ -126,7 +137,7 @@ let union t ~decision_level a b =
     record_undo
       t
       ~decision_level
-      (Merge { child; old_parent = child_term.parent; old_rank = root_term.rank });
+      (Merge { child; old_parent = child_term.parent; old_rank = root_term.rank; reason_var });
     child_term.parent <- root;
     if root_term.rank = child_term.rank then root_term.rank <- root_term.rank + 1;
     record_undo
@@ -141,7 +152,7 @@ let union t ~decision_level a b =
 
 let apply_undo t ({ Undo.decision_level = _; kind } : Undo.t) =
   match kind with
-  | Merge { child; old_parent; old_rank } ->
+  | Merge { child; old_parent; old_rank; reason_var = _ } ->
     let child_term = get_term t child in
     let root_term = get_term t child_term.parent in
     root_term.rank <- old_rank;
@@ -161,9 +172,9 @@ let pop t ~to_decision_level =
   in
   go t.undo_trail;
   t.asserted_eqs
-  <- List.filter t.asserted_eqs ~f:(fun (a, b, _var) -> same_class t a b);
+  <- List.filter t.asserted_eqs ~f:(fun e -> e.decision_level <= to_decision_level);
   t.asserted_neqs
-  <- List.filter t.asserted_neqs ~f:(fun (a, b, _var) -> not (same_class t a b))
+  <- List.filter t.asserted_neqs ~f:(fun e -> e.decision_level <= to_decision_level)
 ;;
 
 let assert_literal t ~decision_level literal =
@@ -174,28 +185,42 @@ let assert_literal t ~decision_level literal =
   | This atom ->
     (match atom, is_positive with
      | Atom.Eq (a, b), true ->
-       t.asserted_eqs <- (a, b, var) :: t.asserted_eqs;
-       ignore (union t ~decision_level a b : bool)
+       t.asserted_eqs <- { a; b; var; decision_level } :: t.asserted_eqs;
+       ignore (union t ~decision_level ~reason_var:var a b : bool)
      | Atom.Neq (a, b), false ->
-       t.asserted_eqs <- (a, b, var) :: t.asserted_eqs;
-       ignore (union t ~decision_level a b : bool)
-     | Atom.Neq (a, b), true -> t.asserted_neqs <- (a, b, var) :: t.asserted_neqs
-     | Atom.Eq (a, b), false -> t.asserted_neqs <- (a, b, var) :: t.asserted_neqs)
+       t.asserted_eqs <- { a; b; var; decision_level } :: t.asserted_eqs;
+       ignore (union t ~decision_level ~reason_var:var a b : bool)
+     | Atom.Neq (a, b), true ->
+       t.asserted_neqs <- { a; b; var; decision_level } :: t.asserted_neqs
+     | Atom.Eq (a, b), false ->
+       t.asserted_neqs <- { a; b; var; decision_level } :: t.asserted_neqs)
 ;;
 
-let find_eq_atom t lhs rhs =
-  match Registry.var t.registry ~atom:(Atom.Eq (lhs, rhs)) with
+(* Walk the undo trail to collect the reason vars for all merges that put
+   [a] and [b] into the same class. Returns a list of eq vars. *)
+let explain_eq t a b =
+  (* We need the vars on the merge path. The undo trail records every merge
+     in reverse chronological order; the merges that matter are those whose
+     child's current root is the same as find(a) or find(b). A simple
+     sound (but possibly over-approximate) approach: collect all Merge
+     reason_vars for the two roots involved. For soundness we include all
+     merges that contributed to the current class of find(a). *)
+  let root = find t a in
+  assert (Term_id.equal root (find t b));
+  List.filter_map t.undo_trail ~f:(fun { Undo.kind; _ } ->
+    match kind with
+    | Merge { child; reason_var; _ } ->
+      if Term_id.equal (find t child) root then Some reason_var else None
+    | Class_list _ -> None)
+;;
+
+let find_eq_var t lhs rhs =
+  match Registry.var t.registry ~atom:(Atom.eq lhs rhs) with
   | This v -> Some v
   | Null ->
-    (match Registry.var t.registry ~atom:(Atom.Eq (rhs, lhs)) with
-     | This v -> Some v
-     | Null ->
-       (match Registry.var t.registry ~atom:(Atom.Neq (lhs, rhs)) with
-        | This v -> Some (-v)
-        | Null ->
-          (match Registry.var t.registry ~atom:(Atom.Neq (rhs, lhs)) with
-           | This v -> Some (-v)
-           | Null -> None)))
+    (match Registry.var t.registry ~atom:(Atom.neq lhs rhs) with
+     | This v -> Some (-v)
+     | Null -> None)
 ;;
 
 let congruent t ta tb =
@@ -209,13 +234,11 @@ let congruent t ta tb =
 
 let check_consistent t =
   let conflict =
-    List.find_map t.asserted_neqs ~f:(fun (a, b, neq_var) ->
+    List.find_map t.asserted_neqs ~f:(fun { a; b; var = neq_var; _ } ->
       if same_class t a b
       then (
-        let eq_lits =
-          List.filter_map t.asserted_eqs ~f:(fun (_a, _b, eq_var) -> Some (-eq_var))
-        in
-        Some (Array.of_list (neq_var :: eq_lits)))
+        let path_vars = explain_eq t a b in
+        Some (Array.of_list (neq_var :: List.map path_vars ~f:(fun v -> -v))))
       else None)
   in
   match conflict with
@@ -232,15 +255,18 @@ let check_consistent t =
           let tj = get_term t j in
           if congruent t ti tj && not (same_class t i j)
           then (
-            match find_eq_atom t i j with
+            match find_eq_var t i j with
             | None -> ()
             | Some lit when lit > 0 ->
-              let reason =
-                Array.of_list
-                  (lit
-                   :: List.filter_map t.asserted_eqs ~f:(fun (_a, _b, eq_var) ->
-                     Some (-eq_var)))
+              let arg_eq_vars =
+                match ti.Term.def, tj.Term.def with
+                | App { args = aa; _ }, App { args = ab; _ } ->
+                  Array.to_list aa
+                  |> List.concat_mapi ~f:(fun k arg_i ->
+                    explain_eq t arg_i ab.(k) |> List.map ~f:(fun v -> -v))
+                | _ -> []
               in
+              let reason = Array.of_list (lit :: arg_eq_vars) in
               propagations := (lit, reason) :: !propagations
             | Some _ -> ())
         done
