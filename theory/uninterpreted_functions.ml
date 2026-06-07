319a7b329f5af75fb6422ce1c4f3e488980c595d
Evan Williams <evanawilliams@live.com>
2026-06-07 17:57:21 +0800

    😊

--- theory/uninterpreted_functions.ml
+++ theory/uninterpreted_functions.ml
@@ -88,16 +88,18 @@ type t =
   ; trail : Trail_entry.Vec.t
   ; mutable trail_entry_processed_till : int
   ; falsehoods : Atom.Hash_set.t
+  ; mutable current_decision_level : int
   }
 
 let rec undo t ~to_decision_level_excl =
   match Trail_entry.Vec.length t.trail with
   | 0 -> ()
-  | _ ->
-    let trail_entry = Trail_entry.Vec.pop_exn t.trail in
+  | len ->
+    let trail_entry = Trail_entry.Vec.get t.trail (len - 1) in
     (match trail_entry.#decision_level > to_decision_level_excl with
-     | false -> ()
+     | false -> t.current_decision_level <- trail_entry.#decision_level
      | true ->
+       ignore (Trail_entry.Vec.pop_exn t.trail : Trail_entry.t);
        (match trail_entry.#kind with
         | T #(Falsehood, #(atom, (), ())) -> Hash_set.remove t.falsehoods atom
         | T #(Signature_added, #(signature, (), ())) ->
@@ -158,6 +160,7 @@ let create ~atoms =
   ; trail_entry_processed_till = 0
   ; signature_to_canonical
   ; falsehoods = Atom.Hash_set.create ()
+  ; current_decision_level = 0
   }
 ;;
 
@@ -169,33 +172,83 @@ let canonical_signature_term t ~term : Term.t or_null =
       (Hashtbl.find_or_add
          t.signature_to_canonical
          signature
-         ~default:(fun () -> term))
+         ~default:(fun () ->
+           Trail_entry.Vec.push
+             t.trail
+             #{ decision_level = t.current_decision_level
+              ; kind = Trail_entry.Kind.signature_added ~signature
+              };
+           term)
+      [@nontail])
 ;;
 
-(* let assert_true_atom t ~decision_level ~(atom : Atom.t) = *)
-(* let worklist = Vec.Value.create () in *)
-(* Vec.Value.push worklist atom; *)
-(* let rec go () = *)
-(* match Vec.Value.length worklist with *)
-(* | 0 -> () *)
-(* | _ -> *)
-(* let (`Eq (term1, term2)) = Vec.Value.pop_exn worklist in *)
-(* let t1 = canonical_ufds_var t ~term:term1 in *)
-(* let t2 = canonical_ufds_var t ~term:term2 in *)
-(* (match%optional_u *)
-(* (Ufdsu.union t.ufdsu t1 t2 : Ufdsu.Undo_entry.Option_u.t) *)
-(* with *)
-(* | None -> () *)
-(* | Some undo_entry -> *)
-(* let could_change1, could_change2 = Hashtbl.find in *)
-(* ()) *)
-(* in *)
-(* go () *)
-(* ;; *)
+let assert_true_atom t ~decision_level ~(atom : Atom.t) =
+  let worklist = Vec.Value.create () in
+  Vec.Value.push worklist atom;
+  let rec go () =
+    match Vec.Value.length worklist with
+    | 0 -> ()
+    | _ ->
+      let (`Eq (term1, term2)) = Vec.Value.pop_exn worklist in
+      let t1 = canonical_ufds_var t ~term:term1 in
+      let t2 = canonical_ufds_var t ~term:term2 in
+      (match Ufdsu.same_class t.ufdsu t1 t2 with
+       | true -> ()
+       | false ->
+         let could_change term =
+           Hashtbl.find_or_null t.parents term
+           |> Or_null.map ~f:(fun hash_set -> Hash_set.to_list hash_set)
+           |> Or_null.value ~default:[]
+         in
+         let could_change =
+           could_change term1 @ could_change term2
+           |> List.dedup_and_sort ~compare:[%compare: Term.t]
+         in
+         List.iter could_change ~f:(fun term ->
+           match signature t ~term with
+           | Null -> ()
+           | This signature ->
+             (match
+                Hashtbl.find_and_remove t.signature_to_canonical signature
+              with
+              | None -> ()
+              | Some old_term ->
+                Trail_entry.Vec.push
+                  t.trail
+                  #{ decision_level
+                   ; kind =
+                       Trail_entry.Kind.signature_changed ~signature ~old_term
+                   }));
+         let undo_entry =
+           (Ufdsu.union t.ufdsu t1 t2 : Ufdsu.Undo_entry.Option_u.t)
+           |> Ufdsu.Undo_entry.Option_u.value_exn
+         in
+         Trail_entry.Vec.push
+           t.trail
+           #{ decision_level; kind = Trail_entry.Kind.undo undo_entry };
+         List.iter could_change ~f:(fun term ->
+           match canonical_signature_term t ~term with
+           | Null -> ()
+           | This canonical ->
+             (match
+                Ufdsu.same_class
+                  t.ufdsu
+                  (canonical_ufds_var t ~term)
+                  (canonical_ufds_var t ~term:canonical)
+              with
+              | true -> (* do nothing *) ()
+              | false ->
+                (* need to merge *)
+                Vec.Value.push worklist (`Eq (term, canonical))));
+         go ())
+  in
+  go ()
+;;
 
-(* let assert_atom t ~decision_level ~(atom : Atom.t) ~value = *)
-(* let atom = Atom.normalize atom in *)
-(* match value with *)
-(* | true -> assert_true_atom t ~decision_level ~atom *)
-(* | false -> () *)
-(* ;; *)
+let assert_atom t ~decision_level ~(atom : Atom.t) ~value =
+  t.current_decision_level <- decision_level;
+  let atom = Atom.normalize atom in
+  match value with
+  | true -> assert_true_atom t ~decision_level ~atom
+  | false -> ()
+;;
