d52ae7f1ec65745295468625edeba0adc4f0ea61
Evan Williams <evanawilliams@live.com>
2026-06-07 12:56:47 +0800

    😊

--- theory/uninterpreted_functions.ml
+++ theory/uninterpreted_functions.ml
@@ -50,24 +50,75 @@ module Trail_entry = struct
   include functor Vecable.Make [@kind (value & value & value) & value]
 end
 
+module Signature = struct
+  type t =
+    { function_ : Tvar.t
+    ; args : int list
+    }
+  [@@deriving sexp, compare, hash]
+
+  include functor Hashable.Make
+end
+
 type t =
   { ufds_var_by_term : int Term.Table.t
   ; ufdsu : Ufdsu.t
   ; atoms : Atom_data.t Atom.Table.t
+  ; signature_to_canonical : Term.t Signature.Table.t
+  ; parents : Term.Hash_set.t Term.Table.t
   ; trail : Trail_entry.Vec.t
   ; mutable trail_entry_processed_till : int
   }
 
+let canonical_ufds_var t ~term =
+  Hashtbl.find_exn t.ufds_var_by_term term |> Ufdsu.find t.ufdsu _
+;;
+
+let signature t ~(term : Term.t) : Signature.t or_null =
+  match term with
+  | `Var _ -> Null
+  | `App (~function_, ~args) ->
+    let args = List.map args ~f:(fun term -> canonical_ufds_var t ~term) in
+    This { function_; args }
+;;
+
 let create ~atoms =
-  { ufds_var_by_term = Term.Table.create ()
-  ; ufdsu = Ufdsu.create ()
-  ; atoms =
-      List.map atoms ~f:(fun (atom, sat_var) : (Atom.t * Atom_data.t) ->
-        let atom = Atom.normalize atom in
-        atom, { atom; sat_var; assignment = Null })
-      |> Atom.Table.of_alist_exn
+  let ufds_var_by_term = Term.Table.create () in
+  let signature_to_canonical = Signature.Table.create () in
+  let parents = Term.Table.create () in
+  let ufdsu = Ufdsu.create () in
+  let on_term term =
+    match Hashtbl.find ufds_var_by_term term with
+    | Some _ -> ()
+    | None ->
+      let ufds_var = Ufdsu.add ufdsu in
+      Hashtbl.set ufds_var_by_term ~key:term ~data:ufds_var;
+      (match (term : Term.t) with
+       | `Var _ -> ()
+       | `App (~function_:_, ~args) ->
+         List.iter args ~f:(fun arg ->
+           let parents =
+             Hashtbl.find_or_add parents arg ~default:Term.Hash_set.create
+           in
+           Hash_set.add parents term))
+  in
+  let atoms =
+    List.map atoms ~f:(fun (atom, sat_var) : (Atom.t * Atom_data.t) ->
+      let atom = Atom.normalize atom in
+      (match atom with
+       | `Eq (a, b) ->
+         on_term a;
+         on_term b);
+      atom, { atom; sat_var; assignment = Null })
+    |> Atom.Table.of_alist_exn
+  in
+  { ufds_var_by_term
+  ; ufdsu
+  ; atoms
+  ; parents
   ; trail = Trail_entry.Vec.create ()
   ; trail_entry_processed_till = 0
+  ; signature_to_canonical
   }
 ;;
 
