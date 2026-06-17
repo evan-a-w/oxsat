387b58e9e6525beac9f1b7403532ef79411395c8
Evan Williams <evanawilliams@live.com>
2026-06-17 23:57:44 +0800

    😊

--- theory/tvar_types.ml
+++ theory/tvar_types.ml
@@ -1,4 +1,5 @@
 open! Core
+open! Feel.Import
 
 module Type = struct
   type t =
@@ -15,19 +16,22 @@ type trail_entry =
 
 type t =
   { types : Type.t Tvar.Table.t
-  ; trail : trail_entry list ref
+  ; trail : trail_entry Vec.Value.t
   ; mutable depth : int
   }
 
-let create () = { types = Tvar.Table.create (); trail = ref []; depth = 0 }
+let create () =
+  { types = Tvar.Table.create (); trail = Vec.Value.create (); depth = 0 }
+;;
+
 let push t = t.depth <- t.depth + 1
 
 let pop t =
   t.depth <- t.depth - 1;
   let rec undo () =
-    match !(t.trail) with
-    | entry :: rest when entry.depth > t.depth ->
-      t.trail := rest;
+    match Vec.Value.last t.trail with
+    | Some entry when entry.depth > t.depth ->
+      ignore (Vec.Value.pop_exn t.trail : trail_entry);
       (match entry.old_type with
        | None -> Hashtbl.remove t.types entry.var
        | Some typ -> Hashtbl.set t.types ~key:entry.var ~data:typ);
@@ -39,7 +43,7 @@ let pop t =
 
 let assert_type t var typ =
   let old_type = Hashtbl.find t.types var in
-  t.trail := { depth = t.depth; var; old_type } :: !(t.trail);
+  Vec.Value.push t.trail { depth = t.depth; var; old_type };
   Hashtbl.set t.types ~key:var ~data:typ
 ;;
 
